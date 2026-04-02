f48e99a31379367a2b59220698900eb0e2587169
Tibor Kiss <32801432+the1bit@users.noreply.github.com>
2026-04-02 17:00:26 +0200

    Network module (#7)
    * feat: introduce network module for VPC and security group provisioning in basic lab
    
    * feat: implement network module and integrate into basic lab configuration

--- aws/labs/basic/main.tf
+++ aws/labs/basic/main.tf
@@ -11,5 +11,12 @@ module "trainee" {
   tags             = var.tags
 }
 
+module "network" {
+  source           = "../../modules/network"
+  trainee_username = var.trainee_username
+  aws_region       = var.aws_region
+  tags             = var.tags
+}
+
 
 
--- aws/labs/basic/outputs.tf
+++ aws/labs/basic/outputs.tf
@@ -2,7 +2,14 @@ output "trainee_username" {
   value = module.trainee.trainee_username
 }
 
+output "vpc_id" {
+  value = module.network.vpc_id
+}
 
-output "aws_console_url" {
-  value = "https://${data.aws_caller_identity.current.account_id}.signin.aws.amazon.com/console"
+output "public_subnet_id" {
+  value = module.network.public_subnet_id
+}
+
+output "security_group_id" {
+  value = module.network.security_group_id
 }
