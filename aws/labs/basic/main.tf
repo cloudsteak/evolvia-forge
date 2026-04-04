532b27570e708e5e292d06979fab30eda18dbfbf
the1bit <tibor.kiss@cloudmentor.hu>
2026-04-04 05:03:46 +0200

    feat: remove unused network module from trainee lab configuration

--- aws/labs/basic/main.tf
+++ aws/labs/basic/main.tf
@@ -11,12 +11,5 @@ module "trainee" {
   tags             = var.tags
 }
 
-module "network" {
-  source           = "../../modules/network"
-  trainee_username = var.trainee_username
-  aws_region       = var.aws_region
-  tags             = var.tags
-}
-
 
 
