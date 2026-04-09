033c049db5f1787277d59b5bba33a81678472bc9
the1bit <tibor.kiss@cloudmentor.hu>
2026-04-09 03:21:09 +0200

    refactor: remove unused output variables from outputs.tf files

--- aws/labs/s3-staticwebsite/outputs.tf
+++ aws/labs/s3-staticwebsite/outputs.tf
@@ -1,15 +1,3 @@
 output "trainee_username" {
   value = module.trainee.trainee_username
 }
-
-output "vpc_id" {
-  value = module.network.vpc_id
-}
-
-output "public_subnet_id" {
-  value = module.network.public_subnet_id
-}
-
-output "security_group_id" {
-  value = module.network.security_group_id
-}
